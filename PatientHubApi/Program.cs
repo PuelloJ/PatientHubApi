ca2637717d53ba99dc31bd4a5063038ad95b10f2
PuelloJ <josepuello112000@gmail.com>
2025-09-27 13:24:52 -0500

    Refactor Program.cs to add infrastructure services, CORS policy, and middleware configuration

--- PatientHubApi/Program.cs
+++ PatientHubApi/Program.cs
@@ -1,12 +1,19 @@
 using Microsoft.EntityFrameworkCore;
+using PatientHubApi.API.Middleware;
+using PatientHubApi.Application;
+using PatientHubApi.Infrastructure;
+
 
 // Data Context
-using PatientHubApi.Data;
+using PatientHubApi.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplicationServices();
+
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,6 +22,19 @@ builder.Services.AddDbContext<ApplicationDbContext>(option =>
         builder.Configuration.GetConnectionString("DefaultConnection")
     )
 );
+
+// Configure CORS policy to allow requests from any origin
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "AllowAll", policy =>
+        {
+            policy.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        }
+    );
+});
 
 var app = builder.Build();
 
@@ -25,6 +45,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowAll");
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
 
 app.Run();
